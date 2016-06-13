49938f663a5ca6d62f8da1d868e54c9f8269fd3d
Tirath Ramdas <tirath@int19h.com>
2016-06-13 14:24:05 +0800

    cleanup

--- pull_feed.py
+++ pull_feed.py
@@ -19,12 +19,32 @@ GROUP_ID = "497068793653308"
 logging.basicConfig(level=logging.INFO)
 #logging.basicConfig(level=logging.DEBUG)
 
+def main():
+    # the easiest way to obtain an oauth token is to do it by hand
+    # in https://developers.facebook.com/tools/explorer, then save
+    # the token into the file specified here:
+    oauth_access_token_file = 'oauth_file'
+    with open(oauth_access_token_file, 'r') as oauth_fd:
+        oauth_access_token = oauth_fd.readlines()[0]
+    bdmy = Group(GROUP_ID)
+    bdmy.fetch(oauth_access_token, 3)
+    plt.plot_date(x=[x.updated_date for x in bdmy.posts],
+                  y=[x.get_all_engagements_count() for x in bdmy.posts],
+                  fmt="ro-")
+    plt.title("Engagements (posts, comments, reactions, likes on comments)")
+    plt.ylabel("Number of engagement events")
+    plt.grid(True)
+    plt.show()
+    pdb.set_trace()
+
+
 class Engagement(object):
 
     def __init__(self, raw_info):
         self._raw_info = raw_info
         print("ENGAGEMENT:")
         pprint.pprint(self._raw_info)
+
 
 class Reaction(object):
     reaction_types = set()
@@ -47,6 +67,7 @@ class Reaction(object):
     @classmethod
     def add_seen_reaction_type(cls, reaction_type_string):
         cls.reaction_types.add(str(reaction_type_string).upper())
+
 
 class Comment(object):
     reactions = None
@@ -126,7 +147,7 @@ class Group(object):
     def add_post(self, post):
         self.posts.append(post)
 
-    def fetch(self, oath_access_token, max_pages=None):
+    def fetch(self, oauth_access_token, max_pages=None):
         """
         For testing purposes one may limit max_pages.
         """
@@ -217,109 +238,5 @@ class Group(object):
                     comment_obj.add_reaction(Reaction(comment_reaction_data, is_like=True))
 
 
-            #comments = list(graph.get('/v2.6/{}/comments'.format(post_obj.fb_id), page=True))
-            #logging.debug("comments: %d, %s", len(comments), pprint.pformat(comments))
-
-            #if 'likes' in post:  # is it still 'likes' in the API? not yet reactions?
-            #    post_likes = post['likes']
-            #    if 'next' in post_likes['paging']:
-            #        logging.info("Post %s needs paging for likes", post_obj.url)
-            #if 'comments' in post:
-            #    post_comments = post['comments']
-            #    post_obj
-            #    if 'next' in post_comments['paging']:
-            #        logging.info("Post %s needs paging for comments", post_obj.url)
-
-
-# the easiest way to obtain an oauth token is to do it by hand
-# in https://developers.facebook.com/tools/explorer, then save
-# the token into the file specified here:
-oauth_access_token_file = 'oauth_file'
-with open(oauth_access_token_file, 'r') as oauth_fd:
-	oauth_access_token = oauth_fd.readlines()[0]
-bdmy = Group(GROUP_ID)
-bdmy.fetch(oauth_access_token, 3)
-plt.plot_date(x=[x.updated_date for x in bdmy.posts],
-              y=[x.get_all_engagements_count() for x in bdmy.posts],
-              fmt="ro-")
-plt.title("Engagements (posts, comments, reactions, likes on comments)")
-plt.ylabel("Number of engagement events")
-plt.grid(True)
-plt.show()
-pdb.set_trace()
-
-
-
-#
-#
-#
-## http://stackoverflow.com/questions/16957275/python-to-json-serialization-fails-on-decimal
-#def decimal_default(obj):
-#	if isinstance(obj, decimal.Decimal):
-#		return float(obj)
-#	raise TypeError
-#
-## the easiest way to obtain an oauth token is to do it by hand
-## in https://developers.facebook.com/tools/explorer, then save
-## the token into the file specified here:
-#oauth_access_token_file = 'oauth_file'
-#with open(oauth_access_token_file, 'r') as oauth_fd:
-#	oauth_access_token = oauth_fd.readlines()[0]
-#
-#graph = GraphAPI(oauth_access_token)
-#
-#start_time_ms = int(time.time() * 1000)
-#
-#seq = 1
-#data = graph.get('{}/feed'.format(GROUP_ID), page=True)
-#posts = []
-#for page in data:
-#    print("new page")
-#    if 'data' in page:
-#        print("page has {} posts".format(len(page['data'])))
-#        posts += [p for p in page['data']]
-#        most_recent_post = posts[-1]
-#        most_recent_post_timestamp = most_recent_post['created_time']
-#        print("current accumulated posts count: {}, oldest timestamp: {}".format(len(posts), most_recent_post_timestamp))
-#
-#for post in posts:
-#    post_link = [x['link'] for x in post['actions'] if x['name'] == 'Comment'][0]
-#    if 'comments' in post:
-#        post_comments = post['comments']
-#        if 'next' in post_comments['paging']:
-#            print("Post {} needs paging for comments".format(post_link))
-#    if 'likes' in post:
-#        post_likes = post['likes']
-#        if 'next' in post_likes['paging']:
-#            print("Post {} needs paging for likes".format(post_link))
-#
-#pdb.set_trace()
-##for page in data:
-##	if len(page['data']) > 0:
-##
-##		# page in long comment threads if needed
-##		for post_index in xrange(len(page['data'])):
-##			post = page['data'][post_index]
-##			page_in_id = None
-##			try:
-##				if 'paging' in post['comments']:
-##					page_in_id = post['id']
-##			except KeyError:
-##				pass
-##			if page_in_id:
-##				print("Paging in {}".format(page_in_id))
-##				all_comments = list(graph.get('{}/comments'.format(page_in_id), page=True))
-##				page['data'][post_index]['comments'] = all_comments
-##
-##		page_file_name = "{}/bigdatamy_feed_pages_{}_{}.json".format(SAVE_DIR, start_time_ms, seq)
-##		try:
-##			with open(page_file_name, "w") as page_save_fd:
-##				json.dump(page, page_save_fd, default=decimal_default)
-##			nominal_date = page['data'][0]['created_time']
-##			print("Saved page {} (containing data from around {}) to {}".format(seq, nominal_date, page_file_name))
-##		except Exception:
-##			print("###############################")
-##			pprint.pprint(page)
-##			raise
-##		seq += 1
-##
+if __name__ == "__main__":
+    main()
