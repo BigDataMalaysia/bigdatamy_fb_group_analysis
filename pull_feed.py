0f71e65ab04d673475f5ae40eeec5413482bef1c
Tirath Ramdas <tirath@int19h.com>
2016-07-05 18:52:14 +1000

    WIP: more efficient engagers cum count.

--- pull_feed.py
+++ pull_feed.py
@@ -269,16 +269,44 @@ class Group(object):
                                                          index=self.time_index)
 
         if extra:
-            # TODO this feels horrible, there must be some way to use pandas to extract these cumulative stats. At a minimum,
-            # chop up the posts array as we go along so the list comprehension does not rescan from scratch on every iter :T
-            self.cumulative_engagement = {}
-            for date in self.time_index:
-                unique_engagers = set()
-                engagers_before_this_date = [p.get_all_engager_ids() for p in self.posts if date.to_datetime() > p.updated_date]
-                for engagers in engagers_before_this_date:
-                    for engager in engagers:
-                        unique_engagers.add(engager)
-                self.cumulative_engagement[date] = len(unique_engagers)
+            logging.info("Making time range pairs sequence")
+            time_range_pairs = [(self.time_index[x], self.time_index[x+1]) for x in range(len(self.time_index))[:-1]]
+            unique_engagers_cum_cnt = []
+            seen_engagers = set()
+
+            start_time, end_time = time_range_pairs.pop(0)
+            for post in self.posts:
+                for engager_id in post.get_all_engager_ids():
+                    seen_engagers.add(engager_id)
+                if post.updated_date == end_time:
+                    unique_engagers_cum_cnt = len(seen_engagers)
+                    start_time, end_time = time_range_pairs.pop(0)
+                assert post.updated_date >= start_time
+
+            #posts_tmp = list(self.posts)
+            #for time_index_index in range(len(bdmy.time_index))[:-1]:
+            #    end_time = bdmy.time_index[time_index_index]
+            #    start_time = bdmy.time_index[time_index_index+1]
+            #    posts_in_range = []
+            #    for post_index in len(self.posts)
+            #        current_post = self.posts[post_index]
+            #        # note the logic here is fragile, in particular ordering sensitive
+
+            #        assert current_post.updated_date >= start_time
+            #        assert post.updated_date < end_time
+            #        if post.updated_date == end_time:
+            #            for post in
+
+            ## TODO this feels horrible, there must be some way to use pandas to extract these cumulative stats. At a minimum,
+            ## chop up the posts array as we go along so the list comprehension does not rescan from scratch on every iter :T
+            #self.cumulative_engagement = {}
+            #for date in self.time_index:
+            #    unique_engagers = set()
+            #    engagers_before_this_date = [p.get_all_engager_ids() for p in self.posts if date.to_datetime() > p.updated_date]
+            #    for engagers in engagers_before_this_date:
+            #        for engager in engagers:
+            #            unique_engagers.add(engager)
+            #    self.cumulative_engagement[date] = len(unique_engagers)
 
         self.engagers_engagement_cnt = {}
         for post in self.posts:
